71dbbbe43c3bc7db5c4491d616d0f000b7c77f4d
Ananda Poudel <apoudel@stcloudstate.edu>
2019-05-14 00:11:09 -0500

    Check permutations

---
+++ CheckPermuation.js
@@ -0,0 +1,14 @@
+function isSimilar(str1, str2) {
+  return (
+    str1
+      .split("")
+      .sort()
+      .join() ===
+    str2
+      .split("")
+      .sort()
+      .join()
+  );
+}
+
+console.log(isSimilar("cow", "woadfc"));
